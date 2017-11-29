40c12fbc87b0e282bd2743fdd185ccc99a3f9fdc
Amit Bakshi <ambakshi@gmail.com>
2017-11-29 09:08:24 -0800

    Fix version

--- brew-fpm.rb
+++ brew-fpm.rb
@@ -1,8 +1,7 @@
 class BrewFpm < Formula
   desc "Homebrew command for building fpm packages from installed formulae."
   homepage "https://github.com/ambakshi/brew-fpm"
-  url "https://github.com/ambakshi/brew-fpm.git", :tag => "master"
-#                                                   :revision => "ac9e3885c01dca0616fe3cec1530fc153da3271b"
+  url "https://github.com/ambakshi/brew-fpm.git", :version => "master"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
